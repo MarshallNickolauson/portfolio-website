07ddac63b0b2ab84bd03bd761cb167cc4107e804
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-09-21 19:26:42 -0400

    fonts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@
 function App() {
 
   return (
-    <>
-      App
-    </>
+    <div className="bg-mainGray">
+      <h1 className="text-white">The quick brown fox jumps over the lazy dog. </h1>
+      <h1 className="text-mainOrange font-roboto font-bold">The quick brown fox jumps over the lazy dog. </h1>
+      <h1 className="text-mainOrange font-ropa">The quick brown fox jumps over the lazy dog. </h1>
+    </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        
-      }
+        mainGray: '#31343E',
+        mainOrange: '#F95353'
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        ropa: ['Ropa Sans', 'sans-serif'],
+      },
     },
   },
   plugins: [],
