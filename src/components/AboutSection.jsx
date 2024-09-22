93db2f7aa15cd06929d3bf7ab98158479400932c
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-09-22 15:32:39 -0400

    fixes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { useInView } from 'react-intersection-observer';
-import pic from '../assets/img/about-pic.jpg'
+import pic from '../assets/img/about-pic.jpg';
 
 const AboutSection = () => {
     const { ref: divRef, inView: isVisible } = useInView({ threshold: 0.01 });
+    const [hasBeenViewed, setHasBeenViewed] = useState(false);
+
+    if (isVisible && !hasBeenViewed) {
+        setHasBeenViewed(true);
+    }
 
     return (
         <div className='container mx-auto mt-40 mb-80'>
-            <div ref={divRef}  className="flex flex-row items-center">
-                <div className={`flex-shrink-0 ${isVisible ? 'fade-in-left' : 'opacity-0'}`}>
+            <div ref={divRef} className="flex flex-row items-center">
+                <div className={`flex-shrink-0 ${hasBeenViewed ? 'fade-in-left' : 'opacity-0'}`}>
                     <img
                         src={pic}
                         alt="About"
                         className='w-11/12 max-w-full aspect-square object-cover mx-auto rounded-lg float-right shadow-md shadow-white'
                     />
                 </div>
-                <div className={`flex-1 ml-12 ${isVisible ? 'fade-in-right' : 'opacity-0'}`}>
+                <div className={`flex-1 ml-12 ${hasBeenViewed ? 'fade-in-right' : 'opacity-0'}`}>
                     <div className='space-y-8'>
                         <h1 className="text-mainOrange text-5xl font-roboto font-bold tracking-wider">Who Am I?</h1>
-                        <span className="block w-[80px] h-[2px] bg-mainOrange"></span>
-                        <div className='space-y-8 max-w-full w-10/12'>
-                            <p className="font-ropa text-white tracking-wider text-xl">My full name is Marshall David Nickolauson, and my life’s passion is God’s Word.</p>
+                        <span className="block w-[100px] h-[2px] bg-mainOrange"></span>
+                        <div className='space-y-8 max-w-full w-8/12'>
+                            <p className="font-ropa text-white tracking-wider text-xl">My full name is Marshall David Nickolauson and one of my life’s passions is using technology to help others.</p>
                             <p className="font-ropa text-white tracking-wider text-xl">I’ve always had a strong interest in technology, and in January of 2022, my coding journey began with the goal of designing solutions that could benefit others and make a positive impact.</p>
-                            <p className="font-ropa text-white tracking-wider text-xl">I believe that my faith in God helps me be a better programmer, guiding me to approach challenges with patience, determination, and the desire to bring joy through my work.</p>
+                            <p className="font-ropa text-white tracking-wider text-xl">I love God and I believe that my faith helps me be a better programmer, guiding me to approach challenges with patience, determination, and the desire to bring joy through my work.</p>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AboutSection
+export default AboutSection;
