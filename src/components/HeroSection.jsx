5cb3d4ee51e29224c8699cfd32af1338d15fa20f
marsh <marshnickol101>
2024-09-22 23:07:15 -0400

    mobile navbar and hero

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -12,17 +12,24 @@ const HeroSection = () => {
         return () => clearTimeout(timer);
     }, []);
 
+    const scrollToSection = (id) => {
+        const section = document.getElementById(id);
+        if (section) {
+            section.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <div>
-            <div className='container mx-auto mt-10'>
-                <div className={`flex flex-row justify-between items-center transition-all duration-1000 transform ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}`}>
+            <div className='container mx-auto mt-10 z-0'>
+                <div className={`flex flex-col md:flex-row justify-between items-center transition-all duration-1000 transform ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}`}>
                     <div className={`space-y-16 mb-10 flex-1`}>
                         <div>
-                            <h1 className='text-white font-ropa text-6xl'>Hello, I'm<span className='text-mainOrange font-roboto font-bold ml-3'>Marshall</span></h1>
-                            <p className='text-mainOrange tracking-wider font-roboto font-bold text-lg mt-1'>Full Stack <span className='text-white font-normal tracking-wide'>Developer & Web Designer</span></p>
+                            <h1 className='text-white text-center md:text-left font-ropa text-6xl'>Hello, I'm<span className='text-mainOrange font-roboto font-bold ml-3'>Marshall</span></h1>
+                            <p className='text-mainOrange text-center md:text-left tracking-wider font-roboto font-bold text-lg mt-1'>Full Stack <span className='text-white font-normal tracking-wide'>Developer & Web Designer</span></p>
                         </div>
-                        <p className='text-white tracking-wider text-xl'>I specialize in building fast, scale-able websites and <br /> web apps using the <span className='relative font-bold tracking-wider bg-gradient-to-r from-mainOrange to-blue-400 bg-clip-text text-transparent hover:cursor-pointer'>MERN<span className="absolute left-0 bottom-0 w-full h-[1px] bg-gradient-to-r from-mainOrange to-blue-400"></span></span> stack.</p>
-                        <div className='flex flex-row space-x-2'>
+                        <p className='text-white text-center md:text-left tracking-wider text-xl px-4 md:px-0 md:w-10/12'>I specialize in building fast, scale-able websites and web apps using the <span onClick={() => scrollToSection('skills')} className='relative font-bold tracking-wider bg-gradient-to-r from-mainOrange to-blue-400 bg-clip-text text-transparent hover:cursor-pointer'>MERN<span className="absolute left-0 bottom-0 w-full h-[1px] bg-gradient-to-r from-mainOrange to-blue-400"></span></span> stack.</p>
+                        <div className='flex flex-row space-x-2 justify-center md:float-left'>
                             <button className='font-ropa text-xl text-white px-3 py-2 border border-mainOrangeDark rounded-md hover:bg-mainOrangeDark transition duration-100'>
                                 View Work
                             </button>
@@ -35,7 +42,7 @@ const HeroSection = () => {
                         <img 
                         src={pic} 
                         alt="Marshall"
-                        className='w-8/12 max-w-full object-contain mx-auto rounded-lg float-left shadow-md shadow-white'
+                        className='w-8/12 max-w-full object-contain mx-auto rounded-lg justify-center md:float-left white-shadow'
                         />
                     </div>
                 </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Navbar = () => {
     const [isVisible, setIsVisible] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const hamburgerIconRef = useRef(null);
+    const mobileMenu = useRef(null);
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -10,21 +13,87 @@ const Navbar = () => {
         return () => clearTimeout(timer);
     }, []);
 
+    const closeMenu = () => {
+        setMenuOpen(false);
+        hamburgerIconRef.current.classList.remove('open');
+        mobileMenu.current.classList.remove('translate-y-0', 'opacity-100');
+        mobileMenu.current.classList.add('-translate-y-full', 'opacity-0');
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (menuOpen && mobileMenu.current && !mobileMenu.current.contains(event.target) && !hamburgerIconRef.current.contains(event.target)) {
+                closeMenu();
+            }
+        };
+
+        const handleScroll = () => {
+            if (menuOpen) {
+                closeMenu();
+            }
+        };
+
+        document.addEventListener('mousedown', handleClickOutside);
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, [menuOpen]);
+
+    const toggleHamburgerIcon = () => {
+        setMenuOpen(!menuOpen);
+        if (!menuOpen) {
+            hamburgerIconRef.current.classList.add('open');
+            mobileMenu.current.classList.remove('translate-y-full', 'opacity-0', 'hidden');
+            mobileMenu.current.classList.add('translate-y-0', 'opacity-100');
+        } else {
+            hamburgerIconRef.current.classList.remove('open');
+            mobileMenu.current.classList.remove('translate-y-0', 'opacity-100');
+            mobileMenu.current.classList.add('translate-y-full', 'opacity-0', 'hidden');
+        }
+    };
+
     return (
-        <div className='mt-3'>
-            <nav className={`container mx-auto py-2 transition-all duration-1000 transform ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
-                <div className='flex flex-row justify-between'>
-                    <h1 className='font-roboto font-bold text-3xl text-mainOrange'>{"<MDN />"}</h1>
-                    <div className='space-x-10'>
-                        <a href='#skills' className='font-ropa text-white text-xl hover:underline'>Skills</a>
-                        <a href='#projects' className='font-ropa text-white text-xl hover:underline'>Projects</a>
-                        <a href='#resume' className='font-ropa text-white text-xl hover:underline'>Resume</a>
-                        <a href='#contact' className='font-ropa text-white text-xl hover:underline'>Contact</a>
-                    </div>
+        <div className='relative z-50 mt-3'>
+            <nav className={`container mx-auto py-2 flex justify-between items-center transition-all duration-1000 transform ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+                {/* Logo */}
+                <h1 className='font-roboto font-bold text-3xl text-mainOrange'>{"<MDN />"}</h1>
+
+                {/* Desktop Menu */}
+                <div className='hidden md:flex space-x-10'>
+                    <a href='#skills' className='font-ropa text-white text-xl hover:underline'>Skills</a>
+                    <a href='#projects' className='font-ropa text-white text-xl hover:underline'>Projects</a>
+                    <a href='#resume' className='font-ropa text-white text-xl hover:underline'>Resume</a>
+                    <a href='#contact' className='font-ropa text-white text-xl hover:underline'>Contact</a>
                 </div>
+
+                {/* Hamburger menu icon (small screens) */}
+                <button
+                    ref={hamburgerIconRef}
+                    id="menu-btn"
+                    className="block hamburger md:hidden focus:outline-none z-50"
+                    onClick={toggleHamburgerIcon}
+                >
+                    <span className="hamburger-top"></span>
+                    <span className="hamburger-middle"></span>
+                    <span className="hamburger-bottom"></span>
+                </button>
             </nav>
+
+            {/* Mobile Menu */}
+            <div
+                ref={mobileMenu}
+                className={`fixed top-20 left-0 w-full py-6 bg-darkGrayBackground text-white flex flex-col justify-center items-center transform transition-all duration-500 ease-in-out ${menuOpen ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'}`}
+            >
+                <a href='#skills' className='font-ropa text-white text-2xl mb-6 hover:underline' onClick={toggleHamburgerIcon}>Skills</a>
+                <a href='#projects' className='font-ropa text-white text-2xl mb-6 hover:underline' onClick={toggleHamburgerIcon}>Projects</a>
+                <a href='#resume' className='font-ropa text-white text-2xl mb-6 hover:underline' onClick={toggleHamburgerIcon}>Resume</a>
+                <a href='#contact' className='font-ropa text-white text-2xl hover:underline' onClick={toggleHamburgerIcon}>Contact</a>
+            </div>
         </div>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
