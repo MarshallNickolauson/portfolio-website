435fb2b96c9e0dc92f9e0b06bc290541c380e51c
marsh <marshnickol101>
2024-09-27 19:47:24 -0400

    added: full projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -10,41 +10,64 @@ const ProjectsSection = () => {
         setHasBeenViewed(true);
     }
 
+    const projectsList = [
+        {
+            picture: project1pic,
+            title: "Manage",
+            description: "Manage is a task management tool that helps companies organize, prioritize, and track projects for improved productivity. Another catching sentence that has ...",
+        },
+        {
+            picture: project1pic,
+            title: "Manage",
+            description: "Manage is a task management tool that helps companies organize, prioritize, and track projects for improved productivity. Another catching sentence that has ...",
+        },
+        {
+            picture: project1pic,
+            title: "Manage",
+            description: "Manage is a task management tool that helps companies organize, prioritize, and track projects for improved productivity. Another catching sentence that has ...",
+        },
+        {
+            picture: project1pic,
+            title: "Manage",
+            description: "Manage is a task management tool that helps companies organize, prioritize, and track projects for improved productivity. Another catching sentence that has ...",
+        }
+    ]
+
     return (
-        <div id='projects'>
-            <div className='container mx-auto mb-60'>
+        <div id='projects' className='bg-gray-700 pt-10 lg:pt-2'>
+            <div className='container mx-auto mb-60 mt-0 lg:mt-10'>
                 <div ref={divRef} className={`flex flex-col justify-center items-center ${hasBeenViewed ? 'fade-in-bottom' : ''}`}>
                     <p className='font-ropa text-white tracking-wider text-xl'>-Some Recent Projects</p>
                     <h1 className='mt-4 text-mainOrange text-5xl font-roboto font-bold tracking-wider'>My Work</h1>
                     <span className="mt-7 block w-[180px] h-[2px] bg-mainOrange"></span>
-                    <p className='font-ropa text-white tracking-wider text-xl mt-12'>Click on a project to explore a detailed showcase</p>
+                    <p className='font-ropa text-white tracking-wider text-xl mt-12 mb-5 text-center'>
+                        Each project provides insight to the development processes, showcasing how specific problems were addressed.
+                    </p>
                 </div>
-                <div className='grid grid-cols-1 lg:grid-cols-2 gap-2 mt-10'>
-                    <div className='flex justify-center bg-mainGrayDark rounded-xl white-shadow hover:cursor-pointer w-10/12 mb-4 mx-auto'>
-                        <div className='flex flex-col'>
-                            <img
-                                src={project1pic}
-                                alt="project"
-                                className='w-full rounded-t-xl'
-                            />
-                            <div className='px-3 mt-2 space-y-3'>
-                                <h1 className='text-white font-roboto font-bold tracking-wider text-xl'>Manage</h1>
-                                <p className='font-ropa text-gray-200 tracking-wider text-md'>
-                                    Manage is a task management tool that helps companies organize, prioritize, and track projects for improved productivity. Another catching sentence that has ...
-                                </p>
-                                <p className='font-ropa text-gray-200 tracking-wider text-md underline hover:cursor-pointer pb-2'>Read more</p>
+                <div className='grid grid-cols-1 lg:grid-cols-2 gap-y-5 lg:gap-y-8 mt-10 pb-10'>
+
+                    {projectsList.map((project, projectIndex) => (
+                        <div key={projectIndex} className='flex justify-center bg-mainGray rounded-xl white-shadow hover:cursor-pointer w-11/12 mb-4 mx-auto'>
+                            <div className='flex flex-col'>
+                                <img
+                                    src={project.picture}
+                                    alt="project"
+                                    className='w-full rounded-t-xl'
+                                />
+                                <div className='px-3 mt-3 space-y-3'>
+                                    <h1 className='text-white font-roboto font-bold tracking-wider text-2xl'>
+                                        {project.title}
+                                    </h1>
+                                    <p className='font-ropa text-lg text-gray-200 tracking-wider text-md'>
+                                        {project.description}
+                                    </p>
+                                    <p className='font-ropa text-lg text-gray-200 tracking-wider text-md underline hover:cursor-pointer pb-2'>
+                                        Click to read
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div>
-                        box
-                    </div>
-                    <div>
-                        box
-                    </div>
-                    <div>
-                        box
-                    </div>
+                    ))}
                 </div>
             </div>
         </div>
