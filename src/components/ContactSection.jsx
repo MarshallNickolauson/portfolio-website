361ed06c5597c51b03ab546add02e471fdf1805e
Marshall Nickolauson <97916067+MarshallNickolauson@users.noreply.github.com>
2024-09-27 23:00:18 -0400

    update: styling

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -85,14 +85,14 @@ const ContactSection = () => {
                     </div>
 
                     {/* Social Media Links */}
-                    <div className="flex justify-center space-x-6">
-                        <a href="https://linkedin.com/in/yourprofile" className="text-gray-600 hover:text-blue-600">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <div className="flex justify-center space-x-4">
+                        <a href="https://www.linkedin.com/in/marshall-nickolauson-3a7b04274/" className="text-gray-300 hover:text-gray-400">
+                            <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M19 0h-14c-2.76 0-5 2.24-5 5v14c0 2.76 2.24 5 5 5h14c2.76 0 5-2.24 5-5v-14c0-2.76-2.24-5-5-5zm-11.11 18.22h-3.33v-8.89h3.33v8.89zm-1.67-10.22c-1.11 0-2-.89-2-2 0-1.11.89-2 2-2s2 .89 2 2c0 1.11-.89 2-2 2zm12.22 10.22h-3.33v-4.67c0-1.11-.89-2-2-2s-2 .89-2 2v4.67h-3.33v-8.89h3.33v1.33c.44-.67 1.56-1.33 2.78-1.33 2.22 0 4 1.78 4 4v5.89z" />
                             </svg>
                         </a>
-                        <a href="https://github.com/yourprofile" className="text-gray-600 hover:text-black">
-                            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
+                        <a href="https://github.com/MarshallNickolauson" className="text-gray-300 hover:text-gray-400">
+                            <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M12 .5a11.5 11.5 0 00-3.634 22.416c.574.106.782-.25.782-.555v-2.217c-3.172.692-3.837-1.525-3.837-1.525-.523-1.332-1.28-1.689-1.28-1.689-1.046-.713.08-.698.08-.698 1.155.082 1.76 1.185 1.76 1.185 1.025 1.76 2.692 1.252 3.35.957.106-.741.4-1.252.73-1.54-2.535-.287-5.2-1.268-5.2-5.634 0-1.246.444-2.265 1.176-3.063-.118-.289-.51-1.45.116-3.021 0 0 .956-.307 3.134 1.172a10.72 10.72 0 015.706 0c2.178-1.478 3.134-1.172 3.134-1.172.626 1.571.236 2.732.118 3.021.732.798 1.176 1.817 1.176 3.063 0 4.377-2.671 5.34-5.215 5.624.412.355.784 1.069.784 2.157v3.193c0 .307.21.67.786.555a11.5 11.5 0 00-3.636-22.416z" />
                             </svg>
                         </a>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = () => {
                 <div className={`flex flex-col md:flex-row justify-between items-center transition-all duration-1000 transform ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-8'}`}>
                     <div className={`space-y-16 mb-10 flex-1`}>
                         <div>
-                            <h1 className='text-white text-center md:text-left font-ropa text-6xl'>Hello, I'm<span className='text-mainOrange font-roboto font-bold ml-3'>Marshall</span></h1>
+                            <h1 className='text-white text-center md:text-left font-ropa text-6xl relative right-1'>Hi, I'm<span className='text-mainOrange font-roboto font-bold ml-3'>Marshall</span></h1>
                             <p className='text-mainOrange text-center md:text-left tracking-wider font-roboto font-bold text-lg mt-1'>Full Stack <span className='text-white font-normal tracking-wide'>Developer & Web Designer</span></p>
                         </div>
                         <p className='text-white text-center md:text-left tracking-wider text-xl px-4 md:px-0 md:w-10/12'>I specialize in building fast, scale-able websites and web apps using the <span onClick={() => scrollToSection('skills')} className='relative font-bold tracking-wider bg-gradient-to-r from-mainOrange to-blue-400 bg-clip-text text-transparent hover:cursor-pointer'>MERN<span className="absolute left-0 bottom-0 w-full h-[1px] bg-gradient-to-r from-mainOrange to-blue-400"></span></span> stack.</p>
